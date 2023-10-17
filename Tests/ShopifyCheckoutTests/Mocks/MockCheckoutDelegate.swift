1f72c095246e7ca0b01de5b0af5b8e74035d48a1
Daniel Kift <daniel.kift@shopify.com>
2023-10-17 10:50:17 +0100

    remove trailing

--- Tests/ShopifyCheckoutTests/Mocks/MockCheckoutDelegate.swift
+++ Tests/ShopifyCheckoutTests/Mocks/MockCheckoutDelegate.swift
@@ -27,16 +27,16 @@ import XCTest
 class ExampleDelegate: CheckoutDelegate {
 	func checkoutDidComplete() {
 	}
-	
+
 	func checkoutDidCancel() {
 	}
-	
+
 	func checkoutDidFail(errors: [ShopifyCheckout.CheckoutError]) {
 	}
-	
+
 	func checkoutDidFail(error: ShopifyCheckout.CheckoutError) {
 	}
-	
+
 	func checkoutDidClickContactLink(url: URL) {
 	}
 }
