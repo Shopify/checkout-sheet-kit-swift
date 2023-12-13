9865eef0c03c4e12f70569cf3fbc161de730f507
Cian Buckley <cian.buckley@shopify.com>
2023-12-13 11:47:14 +0100

    remove test templates and autogenerated headers

--- Samples/SwiftUIExample/SwiftUIExample/SwiftUIExampleApp.swift
+++ Samples/SwiftUIExample/SwiftUIExample/SwiftUIExampleApp.swift
@@ -21,13 +21,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-//
-//  SwiftUIExampleApp.swift
-//  SwiftUIExample
-//
-//  Created by Cian Buckley on 28/11/23.
-//
-
 import SwiftUI
 
 @main
