36b95366b49f6bd472bad2e5e2ed494c823b54f4
José Miguel Álvarez <jose.alvarez@shopify.com>
2023-12-19 14:48:45 +0100

    Fix linting errors

--- Sources/ShopifyCheckoutKit/AnalyticsEvents.swift
+++ Sources/ShopifyCheckoutKit/AnalyticsEvents.swift
@@ -1,5 +1,5 @@
 typealias ClientId = String?
-typealias Id = String?
+typealias Identifier = String?
 typealias Timestamp = String?
 
 struct Location {
@@ -104,13 +104,13 @@ struct Cart {
     var totalQuantity: Int?
 }
 
-struct PixelEventsCartViewedData {
+struct CartViewedData {
     var cart: Cart?
 }
 
 struct Attribute {
-    var key: String
-    var value: String
+    var key: String?
+    var value: String?
 }
 
 struct MailingAddress {
@@ -128,269 +128,268 @@ struct MailingAddress {
 }
 
 struct PricingPercentageValue {
-    var percentage: Double
+    var percentage: Double?
 }
 
 struct DiscountApplication {
-    var allocationMethod: String
-    var targetSelection: String
-    var targetType: String
-    var title: String
-    var type: String
-    var value: DiscountValue
+    var allocationMethod: String?
+    var targetSelection: String?
+    var targetType: String?
+    var title: String?
+    var type: String?
+    var value: DiscountValue?
 }
 
 enum DiscountValue {
-    case money(MoneyV2)
-    case percentage(PricingPercentageValue)
+    case money(MoneyV2?)
+    case percentage(PricingPercentageValue?)
 }
 
 struct DiscountAllocation {
-    var amount: MoneyV2
-    var discountApplication: DiscountApplication
+    var amount: MoneyV2?
+    var discountApplication: DiscountApplication?
 }
 
 struct CheckoutLineItem {
-    var discountAllocations: [DiscountAllocation]
-    var id: String
-    var quantity: Int
+    var discountAllocations: [DiscountAllocation]?
+    var id: String?
+    var quantity: Int?
     var title: String?
     var variant: ProductVariant?
 }
 
 struct Order {
-    var id: String
+    var id: String?
 }
 
 struct ShippingRate {
-    var price: MoneyV2
+    var price: MoneyV2?
 }
 
 struct Transaction {
-    var amount: MoneyV2
-    var gateway: String
+    var amount: MoneyV2?
+    var gateway: String?
 }
 
 struct Checkout {
-    var attributes: [Attribute]
+    var attributes: [Attribute]?
     var billingAddress: MailingAddress?
-    var currencyCode: String
-    var discountApplications: [DiscountApplication]
+    var currencyCode: String?
+    var discountApplications: [DiscountApplication]?
     var email: String?
-    var lineItems: [CheckoutLineItem]
+    var lineItems: [CheckoutLineItem]?
     var order: Order?
     var phone: String?
     var shippingAddress: MailingAddress?
     var shippingLine: ShippingRate?
-    var subtotalPrice: MoneyV2
-    var token: String
-    var totalPrice: MoneyV2
-    var totalTax: MoneyV2
-    var transactions: [Transaction]
+    var subtotalPrice: MoneyV2?
+    var token: String?
+    var totalPrice: MoneyV2?
+    var totalTax: MoneyV2?
+    var transactions: [Transaction]?
+}
+struct CheckoutAddressInfoSubmittedData {
+    var checkout: Checkout?
 }
 
-struct PixelEventsCheckoutAddressInfoSubmittedData {
-    var checkout: Checkout
+struct CheckoutCompletedData {
+    var checkout: Checkout?
 }
 
-struct PixelEventsCheckoutCompletedData {
-    var checkout: Checkout
+struct CheckoutContactInfoSubmittedData {
+    var checkout: Checkout?
 }
 
-struct PixelEventsCheckoutContactInfoSubmittedData {
-    var checkout: Checkout
+struct CheckoutShippingInfoSubmittedData {
+    var checkout: Checkout?
 }
 
-struct PixelEventsCheckoutShippingInfoSubmittedData {
-    var checkout: Checkout
-}
-
-struct PixelEventsCheckoutStartedData {
-    var checkout: Checkout
+struct CheckoutStartedData {
+    var checkout: Checkout?
 }
 
 struct Collection {
-    var id: String
-    var productVariants: [ProductVariant]
-    var title: String
+    var id: String?
+    var productVariants: [ProductVariant]?
+    var title: String?
 }
 
-struct PixelEventsCollectionViewedData {
-    var collection: Collection
+struct CollectionViewedData {
+    var collection: Collection?
 }
 
-struct PixelEventsPageViewedData {}
+struct PageViewedData {}
 
-struct PixelEventsPaymentInfoSubmittedData {
-    var checkout: Checkout
+struct PaymentInfoSubmittedData {
+    var checkout: Checkout?
 }
 
-struct PixelEventsProductAddedToCartData {
+struct ProductAddedToCartData {
     var cartLine: CartLine?
 }
 
-struct PixelEventsProductRemovedFromCartData {
+struct ProductRemovedFromCartData {
     var cartLine: CartLine?
 }
 
-struct PixelEventsProductVariantViewedData {
-    var productVariant: ProductVariant
+struct ProductVariantViewedData {
+    var productVariant: ProductVariant?
 }
 
-struct PixelEventsProductViewedData {
-    var productVariant: ProductVariant
+struct ProductViewedData {
+    var productVariant: ProductVariant?
 }
 
 struct SearchResult {
-    var productVariants: [ProductVariant]
-    var query: String
+    var productVariants: [ProductVariant]?
+    var query: String?
 }
 
-struct PixelEventsSearchSubmittedData {
-    var searchResult: SearchResult
+struct SearchSubmittedData {
+    var searchResult: SearchResult?
 }
 
-public struct PixelEventsCartViewed {
+public struct CartViewed {
     var clientId: ClientId?
     var context: Context?
-    var data: PixelEventsCartViewedData?
-    var id: Id?
+    var data: CartViewedData?
+    var id: Identifier?
     var name: String?
     var timestamp: Timestamp?
 }
 
-public struct PixelEventsCheckoutAddressInfoSubmitted {
-    var clientId: ClientId
-    var context: Context
-    var data: PixelEventsCheckoutAddressInfoSubmittedData
-    var id: Id
-    var name: String
-    var timestamp: Timestamp
+public struct CheckoutAddressInfoSubmitted {
+    var clientId: ClientId?
+    var context: Context?
+    var data: CheckoutAddressInfoSubmittedData?
+    var id: Identifier?
+    var name: String?
+    var timestamp: Timestamp?
 }
 
-public struct PixelEventsCheckoutCompleted {
-    var clientId: ClientId
-    var context: Context
-    var data: PixelEventsCheckoutCompletedData
-    var id: Id
-    var name: String
-    var timestamp: Timestamp
+public struct CheckoutCompleted {
+    var clientId: ClientId?
+    var context: Context?
+    var data: CheckoutCompletedData?
+    var id: Identifier?
+    var name: String?
+    var timestamp: Timestamp?
 }
 
-public struct PixelEventsCheckoutContactInfoSubmitted {
-    var clientId: ClientId
-    var context: Context
-    var data: PixelEventsCheckoutContactInfoSubmittedData
-    var id: Id
-    var name: String
-    var timestamp: Timestamp
+public struct CheckoutContactInfoSubmitted {
+    var clientId: ClientId?
+    var context: Context?
+    var data: CheckoutContactInfoSubmittedData?
+    var id: Identifier?
+    var name: String?
+    var timestamp: Timestamp?
 }
 
-public struct PixelEventsCheckoutShippingInfoSubmitted {
-    var clientId: ClientId
-    var context: Context
-    var data: PixelEventsCheckoutShippingInfoSubmittedData
-    var id: Id
-    var name: String
-    var timestamp: Timestamp
+public struct CheckoutShippingInfoSubmitted {
+    var clientId: ClientId?
+    var context: Context?
+    var data: CheckoutShippingInfoSubmittedData?
+    var id: Identifier?
+    var name: String?
+    var timestamp: Timestamp?
 }
 
-public struct PixelEventsCheckoutStarted {
-    var clientId: ClientId
-    var context: Context
-    var data: PixelEventsCheckoutStartedData
-    var id: Id
-    var name: String
-    var timestamp: Timestamp
+public struct CheckoutStarted {
+    var clientId: ClientId?
+    var context: Context?
+    var data: CheckoutStartedData?
+    var id: Identifier?
+    var name: String?
+    var timestamp: Timestamp?
 }
 
-public struct PixelEventsCollectionViewed {
-    var clientId: ClientId
-    var context: Context
-    var data: PixelEventsCollectionViewedData
-    var id: Id
-    var name: String
-    var timestamp: Timestamp
+public struct CollectionViewed {
+    var clientId: ClientId?
+    var context: Context?
+    var data: CollectionViewedData?
+    var id: Identifier?
+    var name: String?
+    var timestamp: Timestamp?
 }
 
-public struct PixelEventsPageViewed {
-    var clientId: ClientId
-    var context: Context
-    var data: PixelEventsPageViewedData
-    var id: Id
-    var name: String
-    var timestamp: Timestamp
+public struct PageViewed {
+    var clientId: ClientId?
+    var context: Context?
+    var data: PageViewedData?
+    var id: Identifier?
+    var name: String?
+    var timestamp: Timestamp?
 }
 
-public struct PixelEventsPaymentInfoSubmitted {
-    var clientId: ClientId
-    var context: Context
-    var data: PixelEventsPaymentInfoSubmittedData
-    var id: Id
-    var name: String
-    var timestamp: Timestamp
+public struct PaymentInfoSubmitted {
+    var clientId: ClientId?
+    var context: Context?
+    var data: PaymentInfoSubmittedData?
+    var id: Identifier?
+    var name: String?
+    var timestamp: Timestamp?
 }
 
-public struct PixelEventsProductAddedToCart {
-    var clientId: ClientId
-    var context: Context
-    var data: PixelEventsProductAddedToCartData
-    var id: Id
-    var name: String
-    var timestamp: Timestamp
+public struct ProductAddedToCart {
+    var clientId: ClientId?
+    var context: Context?
+    var data: ProductAddedToCartData?
+    var id: Identifier?
+    var name: String?
+    var timestamp: Timestamp?
 }
 
-public struct PixelEventsProductRemovedFromCart {
-    var clientId: ClientId
-    var context: Context
-    var data: PixelEventsProductRemovedFromCartData
-    var id: Id
-    var name: String
-    var timestamp: Timestamp
+public struct ProductRemovedFromCart {
+    var clientId: ClientId?
+    var context: Context?
+    var data: ProductRemovedFromCartData?
+    var id: Identifier?
+    var name: String?
+    var timestamp: Timestamp?
 }
 
-public struct PixelEventsProductVariantViewed {
-    var clientId: ClientId
-    var context: Context
-    var data: PixelEventsProductVariantViewedData
-    var id: Id
-    var name: String
-    var timestamp: Timestamp
+public struct ProductVariantViewed {
+    var clientId: ClientId?
+    var context: Context?
+    var data: ProductVariantViewedData?
+    var id: Identifier?
+    var name: String?
+    var timestamp: Timestamp?
 }
 
-public struct PixelEventsProductViewed {
-    var clientId: ClientId
-    var context: Context
-    var data: PixelEventsProductViewedData
-    var id: Id
-    var name: String
-    var timestamp: Timestamp
+public struct ProductViewed {
+    var clientId: ClientId?
+    var context: Context?
+    var data: ProductViewedData?
+    var id: Identifier?
+    var name: String?
+    var timestamp: Timestamp?
 }
 
-public struct PixelEventsSearchSubmitted {
-    var clientId: ClientId
-    var context: Context
-    var data: PixelEventsSearchSubmittedData
-    var id: Id
-    var name: String
-    var timestamp: Timestamp
+public struct SearchSubmitted {
+    var clientId: ClientId?
+    var context: Context?
+    var data: SearchSubmittedData?
+    var id: Identifier?
+    var name: String?
+    var timestamp: Timestamp?
 }
 
 public struct PixelEvents {
-    var cart_viewed: PixelEventsCartViewed
-    var checkout_address_info_submitted: PixelEventsCheckoutAddressInfoSubmitted
-    var checkout_completed: PixelEventsCheckoutCompleted
-    var checkout_contact_info_submitted: PixelEventsCheckoutContactInfoSubmitted
-    var checkout_shipping_info_submitted: PixelEventsCheckoutShippingInfoSubmitted
-    var checkout_started: PixelEventsCheckoutStarted
-    var collection_viewed: PixelEventsCollectionViewed
-    var page_viewed: PixelEventsPageViewed
-    var payment_info_submitted: PixelEventsPaymentInfoSubmitted
-    var product_added_to_cart: PixelEventsProductAddedToCart
-    var product_removed_from_cart: PixelEventsProductRemovedFromCart
-    var product_variant_viewed: PixelEventsProductVariantViewed
-    var product_viewed: PixelEventsProductViewed
-    var search_submitted: PixelEventsSearchSubmitted
+    var cartViewed: CartViewed
+    var checkoutAddressInfoSubmitted: CheckoutAddressInfoSubmitted
+    var checkoutCompleted: CheckoutCompleted
+    var checkoutContactInfoSubmitted: CheckoutContactInfoSubmitted
+    var checkoutShippingInfoSubmitted: CheckoutShippingInfoSubmitted
+    var checkoutStarted: CheckoutStarted
+    var collectionViewed: CollectionViewed
+    var pageViewed: PageViewed
+    var paymentInfoSubmitted: PaymentInfoSubmitted
+    var productAddedToCart: ProductAddedToCart
+    var productRemovedFromCart: ProductRemovedFromCart
+    var productVariantViewed: ProductVariantViewed
+    var productViewed: ProductViewed
+    var searchSubmitted: SearchSubmitted
 }
 
 public typealias CustomEvents: [String: CustomEvent]
