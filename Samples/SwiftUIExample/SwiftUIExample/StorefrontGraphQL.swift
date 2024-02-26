797f4d2ed83d78e31c312cdbd40082c1fc42fd4d
Mark Murray <mark.murray@shopify.com>
2024-02-26 16:25:39 +0000

    Improve SwiftUI sample app (#136)

--- Samples/SwiftUIExample/SwiftUIExample/StorefrontGraphQL.swift
+++ Samples/SwiftUIExample/SwiftUIExample/StorefrontGraphQL.swift
@@ -41,17 +41,14 @@ struct Connection<Node: Decodable>: Decodable {
 
 struct Product: Decodable {
 	let title: String
-
 	let featuredImage: Image?
-
 	let variants: Connection<ProductVariant>
+	let vendor: String
 }
 
 struct ProductVariant: Decodable {
 	let id: String
-
 	let title: String
-
 	let price: Money
 }
 
@@ -61,7 +58,6 @@ struct Image: Decodable {
 
 struct Money: Decodable {
 	let amount: String
-
 	let currencyCode: String
 
 	func formattedString() -> String? {
