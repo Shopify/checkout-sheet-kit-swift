4cd9f4dc930d4c516da452a6db17b3e53cd90e71
Daniel Kift <daniel.kift@shopify.com>
2024-03-05 09:59:47 +0000

    prevent retaining the scripthandler / webview

--- Sources/ShopifyCheckoutSheetKit/CheckoutWebView.swift
+++ Sources/ShopifyCheckoutSheetKit/CheckoutWebView.swift
@@ -102,7 +102,7 @@ class CheckoutWebView: WKWebView {
 		navigationDelegate = self
 
 		configuration.userContentController
-			.add(self, name: CheckoutBridge.messageHandler)
+			.add(MessageHandler(delegate: self), name: CheckoutBridge.messageHandler)
 		isBridgeAttached = true
 	}
 
