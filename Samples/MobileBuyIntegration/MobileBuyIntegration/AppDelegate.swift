1d58fef5970aa0708f1add3eb5ff050cd5f16b67
Mark Murray <mark.murray@shopify.com>
2024-10-11 11:51:42 -0400

    Re-enable preloading

--- Samples/MobileBuyIntegration/MobileBuyIntegration/AppDelegate.swift
+++ Samples/MobileBuyIntegration/MobileBuyIntegration/AppDelegate.swift
@@ -33,7 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 			$0.colorScheme = .automatic
 
 			/// Enable preloading
-			$0.preloading.enabled = false
+			$0.preloading.enabled = true
 
 			/// Optional logger used for internal purposes
 			$0.logger = FileLogger("log.txt")
