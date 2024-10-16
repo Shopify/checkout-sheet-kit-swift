02d796b749520846a987cfd9f82b210c2b22f5f8
Mark Murray <2034704+markmur@users.noreply.github.com>
2024-10-16 13:04:00 -0400

    [Minor] 3.1.0 (#228)
    Minor - 3.1.0

--- Sources/ShopifyCheckoutSheetKit/ShopifyCheckoutSheetKit.swift
+++ Sources/ShopifyCheckoutSheetKit/ShopifyCheckoutSheetKit.swift
@@ -24,7 +24,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 import UIKit
 
 /// The version of the `ShopifyCheckoutSheetKit` library.
-public let version = "3.0.4"
+public let version = "3.1.0"
 
 internal var invalidateOnConfigurationChange = true
 
