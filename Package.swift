f8a6fa0bc9ad6c2f9e422cce05cccc184ad694ac
Kieran Osgood <kieran.osgood@shopify.com>
2025-06-17 16:46:26 +0100

    fix: mistaken rename of .swiftformat to .swift-format

--- Package.swift
+++ Package.swift
@@ -6,19 +6,19 @@ import PackageDescription
 let package = Package(
     name: "ShopifyCheckoutSheetKit",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ShopifyCheckoutSheetKit",
             targets: ["ShopifyCheckoutSheetKit"]
-        ),
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/lukepistrol/SwiftLintPlugin", from: "0.2.2"),
+        .package(url: "https://github.com/lukepistrol/SwiftLintPlugin", from: "0.2.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -31,8 +31,8 @@ let package = Package(
             name: "ShopifyCheckoutSheetKitTests",
             dependencies: ["ShopifyCheckoutSheetKit"],
             plugins: [
-                .plugin(name: "SwiftLint", package: "SwiftLintPlugin"),
+                .plugin(name: "SwiftLint", package: "SwiftLintPlugin")
             ]
-        ),
+        )
     ]
 )
