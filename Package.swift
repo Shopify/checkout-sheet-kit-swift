4231956d36dd84b4a304fd00bd46bffd76045889
Kieran Osgood <kieran.osgood@shopify.com>
2025-06-19 12:05:49 +0100

    fix: remove buysdk as an export

--- Package.swift
+++ Package.swift
@@ -12,10 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ShopifyCheckoutSheetKit",
-            targets: ["ShopifyCheckoutSheetKit"]),
-        .library(
-            name: "BuySDK",
-            targets: ["BuySDK"])
+            targets: ["ShopifyCheckoutSheetKit"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
