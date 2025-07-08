8833f5793ce3b9e83c208dcc83bfb11321e48a61
Kieran Osgood <kieran.osgood@shopify.com>
2025-07-08 09:33:53 +0100

    fix: remove swiftformat from package

--- Package.swift
+++ Package.swift
@@ -22,8 +22,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/lukepistrol/SwiftLintPlugin", from: "0.2.2"),
-        .package(url: "https://github.com/nicklockwood/SwiftFormat.git", from: "0.55.5")
+        .package(url: "https://github.com/lukepistrol/SwiftLintPlugin", from: "0.2.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
