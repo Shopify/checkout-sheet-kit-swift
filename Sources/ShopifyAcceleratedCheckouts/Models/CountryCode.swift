450a3b1dc9660e6af2f54d8328789dedda2b1606
Kieran Osgood <kieran.osgood@shopify.com>
2025-07-09 09:03:05 +0100

    fix: license errors with new files

--- Sources/ShopifyAcceleratedCheckouts/Models/CountryCode.swift
+++ Sources/ShopifyAcceleratedCheckouts/Models/CountryCode.swift
@@ -1,3 +1,26 @@
+/*
+MIT License
+
+Copyright 2023 - Present, Shopify Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 /// The code designating a country/region, which generally follows ISO 3166-1 alpha-2 guidelines.
 /// If a territory doesn't have a country code value in the `CountryCode` enum, then it might be considered a subdivision
 /// of another country. For example, the territories associated with Spain are represented by the country code `ES`,
