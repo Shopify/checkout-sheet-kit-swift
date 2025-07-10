3f10cdbf7426a817db642f52b5015f84fb642b09
Kieran Osgood <kieran.osgood@shopify.com>
2025-07-10 10:22:40 +0100

    fix: remove test comments

--- Tests/ShopifyAcceleratedCheckoutsTests/Wallets/ShopPay/ShopPayCallbackTests.swift
+++ Tests/ShopifyAcceleratedCheckoutsTests/Wallets/ShopPay/ShopPayCallbackTests.swift
@@ -68,7 +68,6 @@ final class ShopPayCallbackTests: XCTestCase {
     // MARK: - Success Callback Tests
 
     func testSuccessCallbackInvoked() async {
-        // Given
         successExpectation = expectation(description: "Success callback should be invoked")
         var callbackInvoked = false
 
@@ -79,29 +78,23 @@ final class ShopPayCallbackTests: XCTestCase {
             }
         )
 
-        // When - Directly invoke the callback
         sut.eventHandlers.checkoutSuccessHandler?()
 
-        // Then
         await fulfillment(of: [successExpectation], timeout: 1.0)
         XCTAssertTrue(callbackInvoked, "Success callback should have been invoked")
     }
 
     func testSuccessCallbackNotInvokedWhenNil() {
-        // Given - No callback set
         XCTAssertNil(sut.eventHandlers.checkoutSuccessHandler)
 
-        // When - Try to invoke nil callback
         sut.eventHandlers.checkoutSuccessHandler?() // Should not crash
 
-        // Then - Should not crash
         XCTAssertTrue(true, "Should not crash when callback is nil")
     }
 
     // MARK: - Error Callback Tests
 
     func testErrorCallbackInvoked() async {
-        // Given
         errorExpectation = expectation(description: "Error callback should be invoked")
         var callbackInvoked = false
 
@@ -112,29 +105,23 @@ final class ShopPayCallbackTests: XCTestCase {
             }
         )
 
-        // When - Directly invoke the callback
         sut.eventHandlers.checkoutErrorHandler?()
 
-        // Then
         await fulfillment(of: [errorExpectation], timeout: 1.0)
         XCTAssertTrue(callbackInvoked, "Error callback should have been invoked")
     }
 
     func testErrorCallbackNotInvokedWhenNil() {
-        // Given - No callback set
         XCTAssertNil(sut.eventHandlers.checkoutErrorHandler)
 
-        // When - Try to invoke nil callback
         sut.eventHandlers.checkoutErrorHandler?() // Should not crash
 
-        // Then - Should not crash
         XCTAssertTrue(true, "Should not crash when callback is nil")
     }
 
     // MARK: - Cancel Callback Tests
 
     func testCancelCallbackInvoked() async {
-        // Given
         cancelExpectation = expectation(description: "Cancel callback should be invoked")
         var callbackInvoked = false
 
@@ -145,80 +132,63 @@ final class ShopPayCallbackTests: XCTestCase {
             }
         )
 
-        // When - Directly invoke the callback
         sut.eventHandlers.checkoutCancelHandler?()
 
-        // Then
         await fulfillment(of: [cancelExpectation], timeout: 1.0)
         XCTAssertTrue(callbackInvoked, "Cancel callback should have been invoked")
     }
 
     func testCancelCallbackNotInvokedWhenNil() {
-        // Given - No callback set
         XCTAssertNil(sut.eventHandlers.checkoutCancelHandler)
 
-        // When - Try to invoke nil callback
         sut.eventHandlers.checkoutCancelHandler?() // Should not crash
 
-        // Then - Should not crash
         XCTAssertTrue(true, "Should not crash when callback is nil")
     }
 
     // MARK: - Delegate Tests
 
     func testCheckoutCompleteCallback() {
-        // Given
         var completeInvoked = false
         sut.eventHandlers = EventHandlers(
             checkoutSuccessHandler: { completeInvoked = true }
         )
 
-        // When - Test complete callback by invoking onComplete directly
         sut.eventHandlers.checkoutSuccessHandler?()
 
-        // Then
         XCTAssertTrue(completeInvoked, "Complete callback should be invoked")
     }
 
     func testCheckoutFailCallback() {
-        // Given
         var failInvoked = false
         sut.eventHandlers = EventHandlers(
             checkoutErrorHandler: { failInvoked = true }
         )
 
-        // When - Test fail callback by invoking onFail directly
         sut.eventHandlers.checkoutErrorHandler?()
 
-        // Then
         XCTAssertTrue(failInvoked, "Fail callback should be invoked")
     }
 
     func testCheckoutCancelCallback() {
-        // Given
         var cancelInvoked = false
         sut.eventHandlers = EventHandlers(
             checkoutCancelHandler: { cancelInvoked = true }
         )
 
-        // When - Test cancel callback by invoking onCancel directly
         sut.eventHandlers.checkoutCancelHandler?()
 
-        // Then
         XCTAssertTrue(cancelInvoked, "Cancel callback should be invoked")
     }
 
     func testCheckoutDidCancelDelegateBehavior() {
-        // Given
         var cancelInvoked = false
         sut.eventHandlers = EventHandlers(
             checkoutCancelHandler: { cancelInvoked = true }
         )
 
-        // When - Test cancel through delegate method
         sut.checkoutDidCancel()
 
-        // Then
         XCTAssertTrue(cancelInvoked, "Cancel callback should be invoked")
     }
 }
