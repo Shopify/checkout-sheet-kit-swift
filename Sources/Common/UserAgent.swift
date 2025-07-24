a0ff1b7593930603df12e2fb5b97733d5e20cf7a
Kieran Osgood <kieran.osgood@shopify.com>
2025-07-24 16:21:42 +0100

    feat: introduce Common product for UserAgent

---
+++ Sources/Common/UserAgent.swift
@@ -0,0 +1,92 @@
+/*
+ MIT License
+
+ Copyright 2023 - Present, Shopify Inc.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import Foundation
+import UIKit
+
+public enum UserAgent {
+    /// Internal API - Do not use directly. For ShopifyCheckoutSheetKit and ShopifyAcceleratedCheckouts use only.
+    public enum EntryPoint: String {
+        case acceleratedCheckouts = "AcceleratedCheckouts"
+        case checkoutSheetKit = "CheckoutSheetKit"
+    }
+
+    /// Internal API - Do not use directly. For ShopifyCheckoutSheetKit and ShopifyAcceleratedCheckouts use only.
+    public enum Platform: String {
+        case iOS
+        case reactNative = "ReactNative"
+    }
+
+    public enum ColorScheme: String, CaseIterable {
+        /// Uses a light, idiomatic color scheme.
+        case light
+        /// Uses a dark, idiomatic color scheme.
+        case dark
+        /// Infers either `.light` or `.dark` based on the current `UIUserInterfaceStyle`.
+        case automatic
+        /// The color scheme presented to buyers using a desktop or mobile browser.
+        case web = "web_default"
+    }
+
+    /// Internal API - Do not use directly. For ShopifyCheckoutSheetKit and ShopifyAcceleratedCheckouts use only.
+    public enum CheckoutType {
+        case standard
+        case recovery
+    }
+
+    private static let version = "3.2.0"
+
+    /// Internal API - Do not use directly. For ShopifyCheckoutSheetKit and ShopifyAcceleratedCheckouts use only.
+    public static let schemaVersion = "8.1"
+
+    private static let baseUserAgent = "ShopifyCheckoutSDK/\(version)"
+
+    /// Internal API - Do not use directly. For ShopifyCheckoutSheetKit and ShopifyAcceleratedCheckouts use only.
+    /// Shared format for CheckoutSheetKit and AcceleratedCheckouts
+    public static func string(
+        type: CheckoutType,
+        colorScheme: ColorScheme,
+        platform: Platform? = nil,
+        entryPoint: EntryPoint? = nil
+    ) -> String {
+        var parameters: String
+        switch type {
+        case .standard:
+            parameters = "\(schemaVersion);\(colorScheme.rawValue);standard"
+        case .recovery:
+            parameters = "noconnect;\(colorScheme.rawValue);standard_recovery"
+        }
+
+        if let entryPoint {
+            parameters.append(";entry:\(entryPoint.rawValue)")
+        }
+
+        var userAgentString = "\(baseUserAgent) (\(parameters))"
+
+        if let platform {
+            userAgentString.append(" \(platform.rawValue)")
+        }
+
+        return userAgentString
+    }
+}
