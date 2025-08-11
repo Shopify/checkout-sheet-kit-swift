b0771fce2344cd364c9801125f5a508459b0da7e
Kieran Osgood <kieran.osgood@shopify.com>
2025-08-11 15:12:04 +0100

    feat: add view-inspector dependency

--- Package.swift
+++ Package.swift
@@ -22,7 +22,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/lukepistrol/SwiftLintPlugin", from: "0.2.2")
+        .package(url: "https://github.com/lukepistrol/SwiftLintPlugin", from: "0.2.2"),
+        .package(url: "https://github.com/nalexn/ViewInspector", from: "0.10.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -46,7 +47,10 @@ let package = Package(
         ),
         .testTarget(
             name: "ShopifyAcceleratedCheckoutsTests",
-            dependencies: ["ShopifyAcceleratedCheckouts"],
+            dependencies: [
+                "ShopifyAcceleratedCheckouts",
+                .product(name: "ViewInspector", package: "ViewInspector")
+            ],
             plugins: [
                 .plugin(name: "SwiftLint", package: "SwiftLintPlugin")
             ]
