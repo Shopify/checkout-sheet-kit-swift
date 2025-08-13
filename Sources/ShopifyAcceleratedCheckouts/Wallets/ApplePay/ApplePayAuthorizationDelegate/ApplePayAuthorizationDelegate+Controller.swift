251e3030b132de1af240fac662638a38e40be4bd
Kieran Osgood <kieran.osgood@shopify.com>
2025-08-13 19:04:55 +0100

    fix: ensure customer is updated in buyer identity for cart flows

--- Sources/ShopifyAcceleratedCheckouts/Wallets/ApplePay/ApplePayAuthorizationDelegate/ApplePayAuthorizationDelegate+Controller.swift
+++ Sources/ShopifyAcceleratedCheckouts/Wallets/ApplePay/ApplePayAuthorizationDelegate/ApplePayAuthorizationDelegate+Controller.swift
@@ -169,7 +169,10 @@ extension ApplePayAuthorizationDelegate: PKPaymentAuthorizationControllerDelegat
             try? await transition(to: .paymentAuthorized(payment: payment))
             let cartID = try pkEncoder.cartID.get()
 
-            if pkDecoder.requiredContactFields.count > 0 {
+            if pkDecoder.requiredContactFields.count > 0
+                || configuration.common.customer?.email != nil
+                || configuration.common.customer?.phoneNumber != nil
+            {
                 try await controller.storefront.cartBuyerIdentityUpdate(
                     id: cartID,
                     input: .init(
