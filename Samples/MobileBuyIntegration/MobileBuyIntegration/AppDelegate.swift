8fad76bde8fa8339a04aae6cf722cb714c30e50d
Mark Murray <mark.murray@shopify.com>
2025-12-18 13:57:45 +0000

    Authenticate cart view

--- Samples/MobileBuyIntegration/MobileBuyIntegration/AppDelegate.swift
+++ Samples/MobileBuyIntegration/MobileBuyIntegration/AppDelegate.swift
@@ -62,7 +62,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             $0.backgroundColor = ColorPalette.backgroundColor
 
             /// Enable preloading
-            $0.preloading.enabled = true
+            $0.preloading.enabled = false
 
             /// Optional logger used for internal purposes
             $0.logger = FileLogger("log.txt")
